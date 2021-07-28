157b362e6c2487f75a6fcc2ef624aab5221a39c8
onkarpriyadarshi <66131221+onkarpriyadarshi@users.noreply.github.com>
2021-07-28 18:58:36 +0530

    Update index.js

--- server/models/index.js
+++ server/models/index.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose');
 mongoose.set('debug', true);
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/vote');
+// mongoose.connect('mongodb://localhost/vote');
+const DB = 'mongodb+srv://onkar:#@8SYG7fvXicB-m@cluster0.8q6ed.mongodb.net/voting?retryWrites=true&w=majority'
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+}).then(() => {
+  console.log('connection successful');
+  
+}).catch((err)=> console.log('no connection'));
+
 
 module.exports.User = require('./user');
 module.exports.Poll = require('./poll');
